ce203eb37023fbcc2a37d751281e9b4634e7f170
LYNON GAPOR <lynonjongapor73@gmail.com>
2019-11-22 22:16:21 +0800

    problem number 37

---
+++ Global_vs_Local_Scope_in_Functions.js
@@ -0,0 +1,25 @@
+/*
+It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+
+In this example:
+
+var someVar = "Hat";
+function myFun() {
+  var someVar = "Head";
+  return someVar;
+}
+The function myFun will return "Head" because the local version of the variable is present.
+
+Add a local variable to myOutfit function to override the value of outerWear with "sweater".
+
+*/
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater"
+  return outerWear;
+}
+
+myOutfit();

59461cf101a5096a382e8fdebf016c28265352b9
LYNON GAPOR <lynonjongapor73@gmail.com>
2020-03-04 20:39:30 +0800

    Use EXPORT to share a Code Block

---
+++ ES6/KYRIL22.js
@@ -0,0 +1,12 @@
+/*
+Use export to Share a Code Block
+*/
+
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  export const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
